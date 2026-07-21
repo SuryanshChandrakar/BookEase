fcda680915986d82116a68c8e6eabe70cbf61e9a
Suryansh Chandrakar <suryanshchandrakar72@gmail.com>
2026-07-21 23:30:26 +0530

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, {type Express} from 'express';
+//import type {Express} from 'express';
+const app:Express=express();
+
+app.get("/health",(_req,res)=>{
+    res.json({
+        status:'ok',
+        timeStamp: new Date().toISOString()
+    })
+});
+
+export {app};
