427440ccc19331efc8813f7b992cf6879750669f
Suryansh Chandrakar <suryanshchandrakar72@gmail.com>
2026-08-19 22:07:51 +0530

    Add Slot Regeneration workflow in Availability and EventType Services

--- src/services/event-types.service.ts
+++ src/services/event-types.service.ts
@@ -45,7 +45,9 @@ export async function updateEventType(hostId: number, id: number, data: UpdateEv
         }
     }
 
-    return update(id, data);
+    const updatedEventType = await update(id, data);
+    await startRegenerateHostSlotsWorkflow({ hostId });
+    return updatedEventType;
 }
 
 
@@ -58,7 +60,9 @@ export async function removeEventType(hostId: number, id: number) {
     if(eventType.hostId !== hostId) {
         throw forbidden('You are not authorized to delete this event type');
     }
-    return remove(id);
+    const removedEventType = await remove(id);
+    await startRegenerateHostSlotsWorkflow({ hostId });
+    return removedEventType;
 }
 
 export async function getEventTypeById(id: number, hostId: number) {
